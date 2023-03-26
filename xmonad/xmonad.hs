32baeb9c94773ded14a928a285c16d996e2ed436
rangila <alekyrylenko@gmail.com>
2023-03-26 21:26:58 +0200

    Strip and adapt to Kde configuration

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -9,19 +9,17 @@
 import Data.Monoid
 import System.Exit
 import XMonad
+import XMonad.Config.Kde
 import XMonad.Actions.CycleWS
 import XMonad.Actions.Submap
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
+import XMonad.Hooks.ManageDocks (avoidStruts, ToggleStruts(..))
 import XMonad.Hooks.ManageHelpers
--- import XMonad.Layout.Fullscreen
 import XMonad.Layout.Gaps
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 import XMonad.Util.Cursor
 import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
-import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.InsertPosition
 
 import XMonad.Layout.ResizableTile
@@ -65,13 +63,17 @@ myLayout =
     maximize
     $ avoidStruts 
     $ spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True 
-    $ gaps [(U, 10), (D, 10), (R, trayerHeight + 4)] 
+    $ gaps [(U, 10), (D, 10), (R, 4)] 
     $ smartBorders
-    $ ifWider 2000 wide_screen narrow_screen
+    $ ifWider 3000 high_resolution_layouts 
+         (ifWider 2500 wide_resolution_layouts vertical_layouts)
   where
     wideTopRightAngleAbsoluteLayout = (layoutN 1 (absBox 1280 0 3840 1440) Nothing $ Simplest)
         $ (layoutN 1 (absBox 0 0 1280 1440) (Just $ absBox 0 0 1280 2160) $ Simplest)
         $ (layoutAll (absBox 0 1440 3840 2160) $ threeColumns)
+    verticalHighResSegmentationLayout = (layoutN 1 (absBox 960 0 1200 2100) (Just $ absBox 0 0 2160 3840) $ Simplest)
+        $ (layoutN 1 (absBox 0 0 960 2100) (Just $ absBox 0 2100 2160 3840) $ Simplest)
+        $ (layoutAll (absBox 0 2100 2160 3840) $ threeColumns)
     -- default tiling algorithm partitions the screen into two panes
     threeColumns = ThreeColMid nmaster delta ratio
     -- The default number of windows in the master pane
@@ -79,9 +81,11 @@ myLayout =
     -- Percent of screen to increment by when resizing panes
     delta = 3 / 100
     -- Default proportion of screen occupied by master pane
-    ratio = 1 / 3
-    wide_screen = avoidFloats $ wideTopRightAngleAbsoluteLayout ||| threeColumns ||| accordionSimpleTabbed
-    narrow_screen = Mirror threeColumns ||| accordionSimpleTabbed
+    ratio = 1 / 2
+    high_resolution_layouts = avoidFloats $ wideTopRightAngleAbsoluteLayout ||| threeColumns ||| accordionSimpleTabbed
+    wide_resolution_layouts = avoidFloats $ threeColumns ||| TwoPane (3/100) (1/2) ||| Full
+    vertical_layouts = verticalHighResSegmentationLayout ||| Mirror threeColumns ||| accordionSimpleTabbed
+
 
 clipFloat :: W.RationalRect -> W.RationalRect
 clipFloat (W.RationalRect x y a b)
@@ -105,15 +109,6 @@ addSupported props = withDisplay $ \dpy -> do
 -- certain contrib modules.
 --
 myTerminal = "urxvt"
-
-setFehBackground =
-    "feh --bg-fill $HOME/uc-manager/wallpapers/minimalistic-animals-deer-artwork-low-poly-1920x1080-72048.jpeg"
-
-trayerHeight = 20 :: Int
-
-launchTrayer =
-    "trayer --edge right --align center --SetDockType true --expand true --width 30 --height " ++
-    show trayerHeight ++ " --transparent true --tint 0xeae0c8 --alpha 0 --monitor primary &"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
@@ -160,8 +155,8 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
     M.fromList $
     -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-    -- launch dmenu
-    , ((modm, xK_a), spawn "rofi -combi-modi window,drun -show combi -modi combi")
+    -- , ((modm, xK_a), spawn "rofi -combi-modi window,drun -show combi -modi combi")
+    , ((modm, xK_a), spawn "krunner")
     -- launch gmrun
     , ((modm .|. shiftMask, xK_a), spawn "dmenu_run -l 10 -fn Hack-30 -b -i")
     -- screenshot select to clipboard
@@ -223,12 +218,10 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
     --
     -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
     -- Quit xmonad
-    , ((modm .|. shiftMask, xK_z), io (exitWith ExitSuccess))
+    , ((modm .|. shiftMask, xK_z), 
+            spawn "dbus-send --print-reply --dest=org.kde.ksmserver /KSMServer org.kde.KSMServerInterface.logout int32:1 int32:0 int32:1")
     -- Restart xmonad
     , ((modm, xK_z), spawn "xmonad --recompile; xmonad --restart")
-    , ((modm .|. controlMask, xK_r), spawn (setFehBackground ++ "; pkill xmobar; xmonad --restart"))
-    -- Run xmessage with a summary of the default keybindings (useful for beginners)
-    , ((modm .|. shiftMask, xK_slash), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
     ] ++
     [ ( (modm, xK_b)
       , submap . M.fromList $
@@ -296,17 +289,6 @@ myManageHook =
         ]
 
 ------------------------------------------------------------------------
--- Event handling
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
--- myEventHook = fullscreenEventHook
-myEventHook = ewmhDesktopsEventHook <+> fullscreenEventHook
-
-------------------------------------------------------------------------
 -- Status bars and logging
 -- Perform an arbitrary action on each internal state change or X event.
 -- See the 'XMonad.Hooks.DynamicLog' extension for examples.
@@ -320,17 +302,10 @@ myLogHook = return ()
 -- per-workspace layout choices.
 --
 -- By default, do nothing.
-myStartupHook = do
-    spawnOnce setFehBackground
-    spawnOnce "xrdb $HOME/.Xresources"
-    spawnOnce "xset -dpms s off"
-    -- spawnOnce "xsetroot -cursor_name left_ptr"
-    setDefaultCursor xC_left_ptr
-    -- spawnOnce "picom --experimental-backends --config $HOME/.xmonad/picom.conf"
-    spawnOnce launchTrayer
-    spawnOnce "nm-applet"
-    --spawnOnce "jack_control start"
-    setFullscreenSupported
+myStartupHook = return ()
+    -- do
+    -- spawnOnce "xset -dpms s off"
+    -- setFullscreenSupported
 
 -- -- setDefaultCursor xC_pirate
 ------------------------------------------------------------------------
@@ -338,18 +313,7 @@ myStartupHook = do
 -- Run xmonad with the settings you specify. No need to modify this.
 --
 main = do
-    xmproc <- spawnPipe "xmobar -x 0 $HOME/.xmonad/xmobar.config"
-    xmonad $ ewmh defaults
-
--- A structure containing your configuration settings, overriding
--- fields in the default config. Any you don't override, will
--- use the defaults defined in xmonad/XMonad/Config.hs
---
--- No need to modify this.
---
-defaults =
-    def
-      -- simple stuff
+    xmonad kde4Config
         { terminal = myTerminal
         , focusFollowsMouse = myFocusFollowsMouse
         , clickJustFocuses = myClickJustFocuses
@@ -363,62 +327,8 @@ defaults =
         , mouseBindings = myMouseBindings
       -- hooks, layouts
         , layoutHook = myLayout
-        , manageHook = insertPosition End Newer <+> myManageHook <+> (isFullscreen --> doFullFloat) <+> manageDocks
-        , handleEventHook = docksEventHook <+> myEventHook
+        , manageHook = manageHook kde4Config <+> myManageHook 
         , logHook = myLogHook
         , startupHook = myStartupHook
         }
 
--- | Finally, a copy of the default bindings in simple textual tabular format.
-help :: String
-help =
-    unlines
-        [ "The default modifier key is 'alt'. Default keybindings:"
-        , ""
-        , "-- launching and killing programs"
-        , "mod-Shift-Enter  Launch xterminal"
-        , "mod-p            Launch dmenu"
-        , "mod-Shift-p      Launch gmrun"
-        , "mod-Shift-c      Close/kill the focused window"
-        , "mod-Space        Rotate through the available layout algorithms"
-        , "mod-Shift-Space  Reset the layouts on the current workSpace to default"
-        , "mod-n            Resize/refresh viewed windows to the correct size"
-        , ""
-        , "-- move focus up or down the window stack"
-        , "mod-Tab        Move focus to the next window"
-        , "mod-Shift-Tab  Move focus to the previous window"
-        , "mod-j          Move focus to the next window"
-        , "mod-k          Move focus to the previous window"
-        , "mod-m          Move focus to the master window"
-        , ""
-        , "-- modifying the window order"
-        , "mod-Return   Swap the focused window and the master window"
-        , "mod-Shift-j  Swap the focused window with the next window"
-        , "mod-Shift-k  Swap the focused window with the previous window"
-        , ""
-        , "-- resizing the master/slave ratio"
-        , "mod-h  Shrink the master area"
-        , "mod-l  Expand the master area"
-        , ""
-        , "-- floating layer support"
-        , "mod-t  Push window back into tiling; unfloat and re-tile it"
-        , ""
-        , "-- increase or decrease number of windows in the master area"
-        , "mod-comma  (mod-,)   Increment the number of windows in the master area"
-        , "mod-period (mod-.)   Deincrement the number of windows in the master area"
-        , ""
-        , "-- quit, or restart"
-        , "mod-Shift-q  Quit xmonad"
-        , "mod-q        Restart xmonad"
-        , "mod-[1..9]   Switch to workSpace N"
-        , ""
-        , "-- Workspaces & screens"
-        , "mod-Shift-[1..9]   Move client to workspace N"
-        , "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3"
-        , "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3"
-        , ""
-        , "-- Mouse bindings: default actions bound to mouse events"
-        , "mod-button1  Set the window to floating mode and move by dragging"
-        , "mod-button2  Raise the window to the top of the stack"
-        , "mod-button3  Set the window to floating mode and resize by dragging"
-        ]
